a8ed04f5e094367e327b9329285fdb4402c65921
Angelica Flores <flores.guada.angelica@gmail.com>
2019-12-16 22:19:33 -0800

    added keyboard funstionality that is not working

--- Space-invaders/JS/space-invaders.js
+++ Space-invaders/JS/space-invaders.js
@@ -1,3 +1,4 @@
+/* setting up our game asthetics and important functionalities*/
 var config = {
     type: Phaser.AUTO,
     width: 800,
@@ -7,7 +8,7 @@ var config = {
         arcade: {
             gravity: { y: 200 }
         }
-    },
+    },// end of physics
     scene: {
         preload: preload,
         create: create,
@@ -15,24 +16,32 @@ var config = {
 
         extend: {
             makePlayer: makePlayer
-        }
-    }
-};
+        }// end of extend
 
+    }// end of scene
+
+};//end of config
+
+//creating variables for the game : player, leftkey and rightkey
 var game = new Phaser.Game(config);
 var player;
+var leftKey;
+var rightKey;
 
+/*  where we load in external files (or “assets”)*/
 function preload ()
 {
-    //this.load.setBaseURL('http://labs.phaser.io');
-
+    this.load.setBaseURL('http://labs.phaser.io');
     //this.load.image('sky', 'assets/skies/space3.png');
+
+    /* creating the sprite image of the ship in the preaload */
     this.load.image('player', 'assets/Ship.png');
     // this.load.image('logo', 'assets/sprites/phaser3-logo.png');
     // this.load.image('red', 'assets/particles/red.png');
 }
 
-function create ()
+/* where we define the GameObjects that are necessary at the start of our game*/
+function create()
 {
     /*this.add.image(400, 300, 'sky');
 
@@ -52,11 +61,20 @@ function create ()
 
     emitter.startFollow(logo);*/
 
-    player = this.makePlayer(this.sys.canvas.width / 2, this.sys.canvas.width.height);
+    //ssetting the left key and right key
+    player = this.makePlayer(this.sys.canvas.width / 2, this.sys.canvas.height);
+    this.leftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.keyCodes.LEFT);
+    rightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 }
-
+/* where we define animation and interaction in our game  */
 function update() {
+    // making th fucntionality of the keybords to work
+    if( rightKey.isDown && player.x < this.sys.canvas.width - player.displayWidth * player.originX){
+        player.x += 5;
 
+    } else if (leftKey.isDown){
+        player.x += 5;
+    }
 }
 //placing a player at (x,y) position
 function makePlayer(x,y){
